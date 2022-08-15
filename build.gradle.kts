321dafabdb08a1848fa8b160e2a9427f2d9fedf1
Alexey Chuprin <a.chuprin@capjack.ru>
2022-08-15 19:03:21 +0300

    jvm 17 kotlin 1.7.10 gradle 7.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-	kotlin("multiplatform") version "1.6.21" apply false
+	kotlin("multiplatform") version "1.7.10" apply false
 	id("ru.capjack.publisher") version "1.0.0"
 	id("ru.capjack.depver") version "1.2.0"
 }
 
 depver {
 	"ru.capjack.tool" {
-		"tool-lang"("1.12.0")
-		"tool-logging"("1.6.0")
-		"tool-utils"("1.8.0")
-		"tool-io"("1.1.0")
+		"tool-lang"("1.13.1")
+		"tool-logging"("1.7.0")
+		"tool-utils"("1.9.0")
+		"tool-io"("1.2.0")
 	}
-	"ch.qos.logback:logback-classic"("1.2.11")
+	"ch.qos.logback:logback-classic"("1.2.+")
 }
 
 subprojects {
@@ -27,7 +27,7 @@ subprojects {
 			configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
 				targets.forEach {
 					if (it is org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget) {
-						it.compilations.all { kotlinOptions.jvmTarget = "11" }
+						it.compilations.all { kotlinOptions.jvmTarget = "17" }
 					}
 				}
 			}
